bdc41f3ee5291108a194fab506dc4748b772f44c
ransayada <46240258+ransayada@users.noreply.github.com>
2020-11-28 13:45:03 +0200

    Update ewokTest, with docs

--- Project02/SPL211/SPL211/src/test/java/bgu/spl/mics/EwokTest.java
+++ Project02/SPL211/SPL211/src/test/java/bgu/spl/mics/EwokTest.java
@@ -12,18 +12,27 @@ class EwokTest {
     public void setUp() throws Exception {
         ewok = new Ewok(1);
     }
-
+    /*Test flow:
+    *make sure that the ewok is avilable
+    */
     @Test
     public void testGetAvailable() {
         assertTrue(ewok.getAvailable());
     }
+    /*Test flow:
+    *make sure that the ewok is avilable to acquire
+    *acquire the ewok and then make sure it cant be acquire againe
+    */
     @Test
     public void testAcquire() {
         assertTrue(ewok.getAvailable());
         ewok.acquire();
         assertFalse(ewok.getAvailable());
     }
-
+    /*Test flow:
+    *make sure that the ewok is not avilable to acquire
+    *releasing the ewok and then make sure it can be acquire 
+    */
     @Test
     public void testRelease() {
         assertFalse(ewok.getAvailable());
