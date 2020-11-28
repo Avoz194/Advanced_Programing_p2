feae1d0c1f77518e89129bc518c678f7e41cac4e
Avoz194 <63339876+Avoz194@users.noreply.github.com>
2020-11-28 13:31:34 +0200

    add import bgu.spl.mics.application.messages.*;

--- Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/services/C3POMicroservice.java
+++ Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/services/C3POMicroservice.java
@@ -1,12 +1,12 @@
 package bgu.spl.mics.application.services;
-
+import bgu.spl.mics.application.messages.*;
 import bgu.spl.mics.MicroService;
 
 
 /**
- * C3POMicroservices is in charge of the handling {@link AttackEvents}.
+ * C3POMicroservices is in charge of the handling {@link AttackEvent}.
  * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
+ * {@link AttackEvent}.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
--- Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
+++ Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
@@ -2,11 +2,11 @@ package bgu.spl.mics.application.services;
 
 
 import bgu.spl.mics.MicroService;
-
+import bgu.spl.mics.application.messages.*;
 /**
- * HanSoloMicroservices is in charge of the handling {@link AttackEvents}.
+ * HanSoloMicroservices is in charge of the handling {@link AttackEvent}.
  * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
+ * {@link AttackEvent}.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
