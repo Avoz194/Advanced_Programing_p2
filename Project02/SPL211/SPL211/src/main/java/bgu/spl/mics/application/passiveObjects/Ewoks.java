42962b64a731a9e25828c00348cd31ada23e3beb
ransayada <46240258+ransayada@users.noreply.github.com>
2020-12-03 16:18:15 +0200

    Update Ewoks.java

--- Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
+++ Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
@@ -40,6 +40,7 @@ public class Ewoks {
 //        if (!(e.getAvailable()) || ewokVector.contains(e)) { //TODO: Replace with Sync
 //            throw new IllegalArgumentException("you can't acquire an ewok that as been allready acquired.");
 //        }
+        sort(ewoks,0,ewoks.length-1);
         for (int i = 0;i<ewoks.length; i++){
             ewokVector.elementAt(ewoks[i]).acquire();
         }
@@ -56,4 +57,70 @@ public class Ewoks {
         }
 
     }
+    //merge-sort algorithm in order to avoid sync problem with acquiring ewoks TODO: is too fancy ?
+    public void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    public void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
 }
