3a7c868bb12c77e01becfeedecefca4a07e42d83
Avoz194 <63339876+Avoz194@users.noreply.github.com>
2020-12-03 12:09:43 +0200

    Lando

--- Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/services/LandoMicroservice.java
+++ Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/services/LandoMicroservice.java
@@ -1,19 +1,37 @@
 package bgu.spl.mics.application.services;
+
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.passiveObjects.Diary;
+
+import bgu.spl.mics.application.messages.*;
+
+import java.sql.Time;
+import java.sql.Timestamp;
 
 /**
  * LandoMicroservice
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
-public class LandoMicroservice  extends MicroService {
+public class LandoMicroservice extends MicroService {
+    private long duration;
 
     public LandoMicroservice(long duration) {
         super("Lando");
+        this.duration = duration;
     }
 
-    @Override
     protected void initialize() {
-       
+        subscribeEvent(BombDestroyerEvent.class, (BombDestroyerEvent event) -> {
+            try {
+                Thread.sleep(duration);
+            } catch (InterruptedException e) {
+            }
+            complete(event, true);
+        });
+        subscribeBroadcast(VictoryBroadcast.class, (param) -> {
+            Timestamp time = new Timestamp(System.currentTimeMillis());
+            Diary.getInstance().setLandoTerminate(time.getTime());
+        });
     }
 }
