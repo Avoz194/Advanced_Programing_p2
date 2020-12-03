7040633ad1c09601272e3742aa85006f5dfd94e4
ransayada <46240258+ransayada@users.noreply.github.com>
2020-12-03 15:38:52 +0200

    Update Diary.java

--- Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -2,6 +2,7 @@ package bgu.spl.mics.application.passiveObjects;
 
 
 import bgu.spl.mics.MicroService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -11,5 +12,97 @@ import bgu.spl.mics.MicroService;
  * Do not add to this class nothing but a single constructor, getters and setters.
  */
 public class Diary {
+     private AtomicInteger totalAttacks ;
+    private long HanSoloFinish;
+    private long C3POFinish;
+    private long R2D2Deactivate;
+    private long LeiaTerminate;
+    private long HanSoloTerminate;
+    private long C3POTerminate;
+    private long R2D2Terminate;
+    private long LandoTerminate;
 
+    private Diary(){
+        totalAttacks = new AtomicInteger(0);
+        HanSoloFinish = System.currentTimeMillis();
+        C3POFinish = System.currentTimeMillis();
+        R2D2Deactivate =System.currentTimeMillis();
+        LeiaTerminate = System.currentTimeMillis();
+        HanSoloTerminate = System.currentTimeMillis();
+        C3POTerminate = System.currentTimeMillis();
+        R2D2Terminate = System.currentTimeMillis();
+        LandoTerminate = System.currentTimeMillis();
+    }
+
+    public AtomicInteger getTotalAttacks() {
+        return totalAttacks;
+    }
+
+    public long getHanSoloFinish() {
+        return HanSoloFinish;
+    }
+
+    public long getC3POFinish() {
+        return C3POFinish;
+    }
+
+    public long getR2D2Deactivate() {
+        return R2D2Deactivate;
+    }
+
+    public long getLeiaTerminate() {
+        return LeiaTerminate;
+    }
+
+    public long getHanSoloTerminate() {
+        return HanSoloTerminate;
+    }
+
+    public long getC3POTerminate() {
+        return C3POTerminate;
+    }
+
+    public long getR2D2Terminate() {
+        return R2D2Terminate;
+    }
+
+    public long getLandoTerminate() {
+        return LandoTerminate;
+    }
+
+    public void setTotalAttacks(AtomicInteger totalAttacks) {
+        this.totalAttacks = totalAttacks;
+    }
+
+    public void setHanSoloFinish(long hanSoloFinish) {
+        HanSoloFinish = hanSoloFinish;
+    }
+
+    public void setC3POFinish(long c3POFinish) {
+        C3POFinish = c3POFinish;
+    }
+
+    public void setR2D2Deactivate(long r2D2Deactivate) {
+        R2D2Deactivate = r2D2Deactivate;
+    }
+
+    public void setLeiaTerminate(long leiaTerminate) {
+        LeiaTerminate = leiaTerminate;
+    }
+
+    public void setHanSoloTerminate(long hanSoloTerminate) {
+        HanSoloTerminate = hanSoloTerminate;
+    }
+
+    public void setC3POTerminate(long c3POTerminate) {
+        C3POTerminate = c3POTerminate;
+    }
+
+    public void setR2D2Terminate(long r2D2Terminate) {
+        R2D2Terminate = r2D2Terminate;
+    }
+
+    public void setLandoTerminate(long landoTerminate) {
+        LandoTerminate = landoTerminate;
+    }
 }
