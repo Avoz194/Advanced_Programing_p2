b43e1879ca6d9a1bf67f00e07f0ac67cd4be8b89
ransayada <46240258+ransayada@users.noreply.github.com>
2020-12-08 11:56:30 +0200

    Update HanSoloMicroservice.java

--- Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
+++ Project02/SPL211/SPL211/src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
@@ -66,9 +66,7 @@ public class HanSoloMicroservice extends MicroService {
     //making array list from a list
     private ArrayList<Integer> array(List<Integer> l) {
         ArrayList<Integer> arr = new ArrayList<>(l.size());
-        for (Integer i:l) {
-            arr.add(i);
-        }
+        arr.addAll(l);
         return arr;
     }
 }
